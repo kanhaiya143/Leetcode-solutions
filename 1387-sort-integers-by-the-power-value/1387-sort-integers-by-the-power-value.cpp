d5f8ceb0da9c57656a427461cd59d684a2ce1311
Kanhaiya Lal <56789544+kanhaiya143@users.noreply.github.com>
2022-02-20 23:44:55 +0530

    Time: 39 ms (68.47%), Space: 8.6 MB (88.69%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
+    int cnt(int val){
+        
+        if(val<2){
+            return 0;
+        }
+        
+        if(val&1)
+            return 1+cnt(val*3+1);
+        return 1+cnt(val/2);
+    }
     int getKth(int lo, int hi, int k) {
 
         vector<pair<int,int>>v;
-        int cnt=0;
+        
         int val;
         for(int i=lo;i<=hi;i++){
-            val=i;
-            cnt=0;
-            while(ceil(log2(val))!=floor(log2(val))){
-    
-                    if(val&1)
-                    val = val*3 + 1;
-                    else
-                    val=val/2;
-                    
-                    cnt++;
-            } 
             
-            cnt += log2(val);
-            
-           
-            v.push_back({cnt,i});
+            v.push_back({cnt(i),i});
         }
        
         sort(v.begin(),v.end());
