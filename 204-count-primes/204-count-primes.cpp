fef0590c4240966c269d677e840a703f922864c8
Kanhaiya Lal <56789544+kanhaiya143@users.noreply.github.com>
2022-03-03 01:22:13 +0530

    Time: 893 ms (5.03%), Space: 130.6 MB (7.50%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        
+        int count=0;
+        vector<int>dp(n+1,0);
+        
+       for(int i=2;i<=sqrt(n);i++){
+           
+           if(dp[i]==0){
+                for(int j=2*i;j<=n;j+=i){
+                dp[j]=1;
+              }
+           }
+       }
+       
+        for(int i=2;i<n;i++){
+            if(dp[i]==0)
+                count++;
+        }
+        return count;
+    }
+};
