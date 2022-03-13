661acf8cec3e8829f7fc4f45b831316b7e0de3e8
Kanhaiya Lal <56789544+kanhaiya143@users.noreply.github.com>
2022-03-13 17:46:21 +0530

    Time: 8 ms (64.14%), Space: 8.6 MB (66.79%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -4,12 +4,6 @@ public:
         
         int n=triangle.size();
     
-        
-        int dp[n];
-        for(int i=0;i<n;i++){
-            dp[i]=triangle[n-1][i];
-            
-        }
         for(int i=1;i<n;i++){
           
             for(int j=0;j<triangle[i].size();j++){
@@ -20,18 +14,11 @@ public:
                     triangle[i][j]+=triangle[i-1][triangle[i-1].size()-1];
                 }
                 else{
-                    //cout<<i<<" "<<j<<endl;
                     triangle[i][j]+=min(triangle[i-1][j-1],triangle[i-1][j]);
                 }
-                
-                    
+  
             }
         }
-        
-//         for(int i=n-2;i>=0;i--){
-//             for(int j=0;j<triangle[i].size();j++)
-//             dp[j]=min(dp[j],dp[j+1])+triangle[i][j];
-//         }
         
         return *min_element(triangle[n-1].begin(),triangle[n-1].end());
     }
