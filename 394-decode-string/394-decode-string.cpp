39db0aa10638832ad331f152580b2aa7187732d5
Kanhaiya Lal <56789544+kanhaiya143@users.noreply.github.com>
2022-03-19 17:28:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (98.59%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        int n=s.size();
+        stack<char>st;
+        
+        string ans="";
+        for(int i=0;i<n;i++){
+            
+            if(s[i]!=']'){
+               
+                st.push(s[i]);
+            }
+            
+            else{
+                string temp="";
+                while(!st.empty() && st.top()!='['){
+                    temp+=st.top();
+                    st.pop();
+                }
+                cout<<temp<<endl;
+                reverse(temp.begin(),temp.end());
+                string num="";
+                st.pop();
+                while(!st.empty() && st.top()>='0' && st.top()<='9'){
+                    num+=st.top();
+                    st.pop();
+                }
+                cout<<num<<endl;
+                reverse(num.begin(),num.end());
+                int x=stoi(num);
+                
+                for(int i=0;i<x;i++){
+                    for(int j=0;j<temp.size();j++){
+                        st.push(temp[j]);
+                    }
+                }
+                
+                
+            }
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
