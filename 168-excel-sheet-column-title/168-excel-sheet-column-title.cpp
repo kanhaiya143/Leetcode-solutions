3dd27192e7c635a3a430567f723f1e1d0aee4f10
Kanhaiya Lal <56789544+kanhaiya143@users.noreply.github.com>
2022-03-29 00:01:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (45.71%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        
+        string ans="";
+        while(n>0){
+            n-=1;
+            ans+=(n%26 + 'A');
+            n/=26;
+        }
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
