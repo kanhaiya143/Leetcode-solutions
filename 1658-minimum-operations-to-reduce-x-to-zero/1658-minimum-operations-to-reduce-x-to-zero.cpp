c267b347ea9edd5089536147278fcf538f544a64
Kanhaiya Lal <56789544+kanhaiya143@users.noreply.github.com>
2022-03-29 22:58:10 +0530

    Time: 533 ms (33.36%), Space: 153.5 MB (32.72%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, int x, int start, int end, int &mini, int count, int n){
+//     void solve(vector<int>& nums, int x, int start, int end, int &mini, int count, int n){
         
-        if(start>=n || end<0)
-        {
-            return;
-        }
+//         if(start>=n || end<0)
+//         {
+//             return;
+//         }
         
-        if(x==0){
-            mini=min(mini,count);
-            return;
-        }
+//         if(x==0){
+//             mini=min(mini,count);
+//             return;
+//         }
         
-        solve(nums, x-nums[start], start+1, end, mini, count+1, n);
-        solve(nums, x-nums[end], start, end-1, mini, count+1, n);
+//         solve(nums, x-nums[start], start+1, end, mini, count+1, n);
+//         solve(nums, x-nums[end], start, end-1, mini, count+1, n);
         
-        return;
-    }
+//         return;
+//     }
     int minOperations(vector<int>& nums, int x) {
         
         int n=nums.size();
-        int start=0,end=n-1;
-        int mini=INT_MAX;
         int sum=0;
-        int count=0;
-        //solve(nums,x,start,end, mini, count,n);
         
         for(int i=0;i<n;i++){
             sum+=nums[i];
@@ -34,10 +30,12 @@ public:
         
         if(target==0)
             return n;
+        
         unordered_map<int,int>mp;
         
         int maxi=INT_MIN;
         sum=0;
+        
         for(int i=0;i<n;i++){
             sum+=nums[i];
             
