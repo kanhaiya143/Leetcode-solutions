b40d4a5c2dfed1dca5b3cb4c881a7803e21e8759
Kanhaiya Lal <56789544+kanhaiya143@users.noreply.github.com>
2022-04-06 17:14:03 +0530

    Time: 1624 ms (5.09%), Space: 30.9 MB (12.02%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
-    int maxProduct(vector<string>& w) {
+    int maxProduct(vector<string>& str) {
+  map<string, vector<int>>mp;
+ 
+    int n=str.size();
+    
+    for(int i=0;i<n;i++){
+        vector<int>temp(26);
+        for(int j=0;j<str[i].size();j++){
+            temp[str[i][j]-'a']++;
+        }
+        mp[str[i]]=temp;
+      
+        temp.clear();
+    }
+ 
+    int maxi=0;
+    int flag=0;
         
-        int n=w.size();
-        int maxi=0;
-        unordered_set<char>st;
-        int flag=0;
-        
-        for(int i=0;i<n;i++){
-            st.clear();
-            for(int k=0;k<w[i].size();k++){
-                    st.insert(w[i][k]);
-            }
-            for(int j=0;j<n;j++){  
-                flag=0;
-                for(int k=0;k<w[j].size();k++){
-                    if(st.find(w[j][k])!=st.end()){
-                        flag=1;
-                        break;}
-                } 
-                if(flag==0){
-                    if(w[i].size()*w[j].size() > maxi)
-                    maxi=w[i].size()*w[j].size();
+     vector<int>a,b;
+    for(int i=0;i<n-1;i++){
+       
+        for(int j=i+1;j<n;j++){
+            flag=0;
+            a = mp[str[i]];
+            b = mp[str[j]];
+            
+            for(int k=0;k<26;k++){
+                
+                if(a[k]>0 && b[k]>0){
+                    flag=1;
+                    break;
                 }
+            }
+            if(flag==0){
+                if(maxi<(str[i].size())*(str[j].size()))
+                maxi=(str[i].size())*(str[j].size());
             }
             
         }
+    }
         return maxi;
         
     }
