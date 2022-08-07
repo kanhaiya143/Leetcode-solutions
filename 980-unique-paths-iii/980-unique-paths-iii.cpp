344465a85fd1b3adfed1795fadcc68175be02d46
Kanhaiya Lal <56789544+kanhaiya143@users.noreply.github.com>
2022-08-07 12:50:11 +0530

    Time: 5 ms (54.51%), Space: 6.9 MB (92.37%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.cpp
+++ 980-unique-paths-iii/980-unique-paths-iii.cpp
@@ -3,50 +3,47 @@ public:
     int xinc[4]={0,0,1,-1};
     int yinc[4]={1,-1,0,0};
     
-    void solve(int n, int m, int x, int y, vector<vector<int>>&grid, int &count, int &count1){
+    void solve(int n, int m, int x, int y, vector<vector<int>>&grid, int &count, int &count0){
         if(x<0 || y<0 || x>=n || y>=m || grid[x][y]==-1)
             return;
         
-       // cout<<count1<<endl;
-        
         if(grid[x][y]==2){
-            if(count1==-1)
+            if(count0==-1)
             count++;
             
             return;
         }
         
-        count1--;
+        count0--;
         grid[x][y]=-1;
         
         for(int i=0;i<4;i++){
            
-            solve(n,m,x+xinc[i],y+yinc[i],grid,count, count1);
+            solve(n, m, x+xinc[i], y+yinc[i], grid, count, count0);
         }
         grid[x][y]=0;
-        count1++;
+        count0++;
     }
     int uniquePathsIII(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         
         int count=0;
-        int count1=0;
+        int count0=0;
+        int x,y;
          for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==0){
-                    count1++;
+                    count0++;
+                }
+                else if(grid[i][j]==1){
+                    x=i;
+                    y=j;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    solve(n, m, i, j, grid, count, count1);
-                }
-            }
-        }
-        
+
+        solve(n, m, x, y, grid, count, count0);
         return count;
     }
 };
